7575d8dba6013741da6d44cd5138e2d92591b2b8
amalikrigger <amalikrigger93@gmail.com>
2023-10-27 10:28:13 -0400

    first commit

---
+++ WeatherForecastApp/WeatherForecastAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastAppApp.swift
+//  WeatherForecastApp
+//
+//  Created by Amali Krigger on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
